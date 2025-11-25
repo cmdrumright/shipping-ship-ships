bf9990c3748c3743a4519e7b59100297a14abd36
cmdrumright <cdrumright@protonmail.com>
2025-11-25 15:34:50 -0600

    added state data and event listener to hauler module for showing ship count on click

--- scripts/database.js
+++ scripts/database.js
@@ -13,12 +13,12 @@ const database = {
     ships: [
         { id: 1, name: "Esso Atlantic", haulerId: 1 },
         { id: 2, name: "Prairial", haulerId: 1 },
-        { id: 3, name: "Palais Royal", haulerId: 1 },
-        { id: 4, name: "Rivoli", haulerId: 1 },
-        { id: 5, name: "Champs Élysée", haulerId: 1 },
-        { id: 6, name: "Ever Ace", haulerId: 1 },
-        { id: 7, name: "Nisei Maru", haulerId: 1 },
-        { id: 8, name: "MSC Gülsün", haulerId: 1 },
+        { id: 3, name: "Palais Royal", haulerId: 2 },
+        { id: 4, name: "Rivoli", haulerId: 3 },
+        { id: 5, name: "Champs Élysée", haulerId: 2 },
+        { id: 6, name: "Ever Ace", haulerId: 2 },
+        { id: 7, name: "Nisei Maru", haulerId: 3 },
+        { id: 8, name: "MSC Gülsün", haulerId: 3 },
         { id: 9, name: "HMM Rotterdam", haulerId: 1 },
         { id: 10, name: "CMA CGM Trocadero", haulerId: 1 }
     ]
--- scripts/haulerList.js
+++ scripts/haulerList.js
@@ -1,5 +1,20 @@
-import { getHaulers } from "./database.js"
+import { getHaulers, getShips } from "./database.js"
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+
+        // Was a hauler list item clicked?
+        if (itemClicked.dataset.type === "hauler") {
+            // Get the id of the hauler clicked
+            const haulerId = parseInt(itemClicked.dataset.id)
+            const allShips = getShips()
+            let shipCount = allShips.filter((ship) => ship.haulerId === haulerId).length
+            window.alert(`The hauler is carring ${shipCount} shipping ships`)
+        }
+    }
+)
 
 export const haulerList = () => {
     const haulers = getHaulers()
@@ -9,7 +24,7 @@ export const haulerList = () => {
     for (const hauler of haulers) {
         // Convert each hauler object to an <li> and append to the haulersHTML string
         haulersHTML += `
-            <li>
+            <li data-type="hauler" data-id="${hauler.id}">
                 ${hauler.name}
             </li>
         `
