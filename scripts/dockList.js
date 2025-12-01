6728e8604001429e6044d7408d641a81252de38d
cmdrumright <cdrumright@protonmail.com>
2025-12-01 09:44:06 -0600

    added dock state date and click event to show assigned haulers

--- scripts/dockList.js
+++ scripts/dockList.js
@@ -1,5 +1,29 @@
-import { getDocks } from "./database.js"
+import { getDocks, getHaulers } from "./database.js"
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        // if clicked type is dock then get dock id and filter list of haulers
+        if (itemClicked.dataset.type = "dock") {
+
+            const clickedDockId = parseInt(itemClicked.dataset.id)
+            const clickedDockLocation = itemClicked.dataset.location
+            const allHaulers = getHaulers()
+            const dockedHaulers = allHaulers.filter((hauler) => hauler.dockId === clickedDockId)
+            
+            // check if dockedHaulers array has length and display message
+            let haulerString = ""
+            if (dockedHaulers.length === 0) {
+                haulerString = "nothing"
+            } else {
+                haulerString = dockedHaulers.map(hauler => hauler.name).join(", ")
+            }
+
+            window.alert(`The ${clickedDockLocation} dock is currently unloading ${haulerString}`)
+        }
+    }
+)
 
 export const dockList = () => {
     const docks = getDocks()
@@ -9,7 +33,7 @@ export const dockList = () => {
     for (const dock of docks) {
         // Convert each dock object to an <li> and append to the docksHTML string
         docksHTML += `
-            <li>
+            <li data-type="dock" data-id="${dock.id}" data-location="${dock.location}">
                 ${dock.location} can hold ${dock.volume} million tons of cargo
             </li>
         `
